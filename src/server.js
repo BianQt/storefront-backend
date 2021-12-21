ad4362ae05a2e41fbf00b1c57cf98b71067ca558
BianQt <bayanqutshan@gmail.com>
2021-12-22 01:32:32 +0200

    add cors

--- src/server.js
+++ src/server.js
@@ -2,13 +2,14 @@
 
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const notFoundHandler = require("./error-handlers/404");
 const errorHandler = require("./error-handlers/500");
 const logger = require("./middleware/logger");
 const moviesRouter = require('./routes/items.route');
 const customersRouter = require('./routes/categories.route');
 
-
+app.use(cors());
 app.use(logger);
 app.use(express.json());
 app.use(moviesRouter);
